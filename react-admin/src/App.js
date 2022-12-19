d96ac26d3ae6b71b62f1a385124b93ac26ec8303
Fauriis <dobreflorentina58@gmail.com>
2022-12-19 16:42:41 +0200

    app

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,5 +1,53 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import {Route, Routes} from 'react-router-dom'
+import Topbar from './scenes/global/Topbar'
+import Dashboard from './scenes/dashboard'
+import SidebarPro from "./scenes/global/Sidebar";
+// import Team from './scenes/team'
+// import Invoices from './scenes/invoices'
+// import Contacts from './scenes/contacts'
+// import Bar from './scenes/bar'
+// import Form from './scenes/form'
+// import Line from './scenes/line'
+// import FAQ from './scenes/faq'
+// import Geography from './scenes/geography'
+// import Pie from './scenes/pie'
+// import Calendar from './scenes/calendar'
+
+
+
+
 function App() {
-  return <div className="app">HELOOO</div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline></CssBaseline>
+        <div className="app">
+          <SidebarPro></SidebarPro>
+          <main className="content">
+            <Topbar></Topbar>
+            {/* <Routes>
+              <Route path="/" element={<Dashboard></Dashboard>}></Route>
+              <Route path="/team" element={<Team></Team>}></Route>
+              <Route path="/contacts" element={<Contacts></Contacts>}></Route>
+              <Route path="/invoices" element={<Invoices></Invoices>}></Route>
+              <Route path="/form" element={<Form></Form>}></Route>
+              <Route path="/bar" element={<Bar></Bar>}></Route>
+              <Route path="/pie" element={<Pie></Pie>}></Route>
+              <Route path="/line" element={<Line></Line>}></Route>
+              <Route path="/faq" element={<FAQ></FAQ>}></Route>
+              <Route path="/geography" element={<Geography></Geography>}></Route>
+              <Route path="/calendar" element={<Calendar></Calendar>}></Route>
+            </Routes> */}
+          </main>
+        </div>
+        
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
